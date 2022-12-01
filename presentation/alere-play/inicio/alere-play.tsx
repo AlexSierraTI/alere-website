011ed39a25d443f838f2225733730db32e78b5bc
AlexSierraTI <alexandre.sdo@gmail.com>
2022-12-01 15:20:57 -0300

    fix: empty files

---
+++ presentation/alere-play/inicio/alere-play.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Col } from 'antd'
+
+const AlerePlay: React.FC = () => {
+  return (
+    <Col xl={24} md={24} sm={24}>
+    </Col>
+  )
+}
+
+export default AlerePlay
